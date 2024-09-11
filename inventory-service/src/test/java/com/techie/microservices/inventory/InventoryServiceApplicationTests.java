70b8abd86f9e2d35aee85745a06d0392c0027efd
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-09-11 10:34:31 +0400

    feat: added inventory-service

--- inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
