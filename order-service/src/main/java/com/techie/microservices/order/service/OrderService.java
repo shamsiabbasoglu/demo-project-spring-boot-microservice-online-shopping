92537f0f83fc9602f3d59369b0e74256460dcc22
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-09-11 10:30:38 +0400

    feat: added order-service

---
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -0,0 +1,28 @@
+package com.techie.microservices.order.service;
+
+import com.techie.microservices.order.dto.OrderRequest;
+import com.techie.microservices.order.model.Order;
+import com.techie.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        // map OrderRequest to Order object
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .price(orderRequest.price())
+                .skuCode(orderRequest.skuCode())
+                .quantity(orderRequest.quantity())
+                .build();
+
+        // save order to OrderRepository
+        orderRepository.save(order);
+    }
+}
---
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -0,0 +1,58 @@
+package com.techie.microservices.order;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Service;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderServiceApplicationTests {
+
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+    @LocalServerPort
+    Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Test
+    void shouldSubmitOrder() {
+        String requestBody = """
+                {
+                    "skuCode": "iPhone_15",
+                    "price": 1000,
+                    "quantity": 1
+                }
+                """;
+
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        assertThat(responseBodyString, Matchers.is("Order placed successfully"));
+    }
+}
