3bc3cc2706bfe44a03686c69e189f850889ee7d3
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-09-11 10:31:20 +0400

    feat: added order-service

--- order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Service;
 import org.testcontainers.containers.MySQLContainer;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
